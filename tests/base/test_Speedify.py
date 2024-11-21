aa6fe39acd6c94978e512b6cc5f802479e433345
Kai Ebira <120698673+kaiebira@users.noreply.github.com>
2024-11-21 16:02:41 -0700

    Update test_Speedify.py
    gpu-enabled speedify

--- tests/base/test_Speedify.py
+++ tests/base/test_Speedify.py
@@ -9,12 +9,14 @@
 import numpy as np
 from latqcdtools.testing import print_results, concludeTest
 import latqcdtools.base.logger as logger
-from latqcdtools.base.speedify import parallel_function_eval, parallel_reduce, compile, numbaON, \
+from latqcdtools.base.speedify import parallel_function_eval, parallel_reduce, compile, numbaON, jaxON, multiprocessingON, \
     DEFAULTTHREADS
 
 logger.set_log_level('INFO')
 
-numbaON()
+#jaxON()
+#numbaON()
+multiprocessingON(DEFAULTTHREADS)
 
 @compile
 def square(x):
@@ -29,7 +31,7 @@ def testSpeedify():
 
     lpass = True
 
-    temp = parallel_function_eval(square,testArray,nproc=DEFAULTTHREADS)
+    temp = parallel_function_eval(square,testArray)
 
     sum1 = 0.
     for i in testArray:
@@ -38,10 +40,10 @@ def testSpeedify():
 
     lpass *= print_results(sum1,sum2,text='parallel square')
 
-    sum3 = parallel_reduce(square,testArray,nproc=DEFAULTTHREADS)
+    sum3 = parallel_reduce(square,testArray)
     lpass *= print_results(sum2,sum3,text='parallel square reduction')
 
-    temp = parallel_function_eval(power,testArray,args=(3,),nproc=DEFAULTTHREADS)
+    temp = parallel_function_eval(power,testArray,args=(3,))
 
     sum1 = 0.
     for i in testArray:
